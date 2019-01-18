72947ffe734517a75a0859e427499737e73817da
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-18 16:20:16 +0100

    Add getters/setters in Customer

--- src/com/mlenoir/homeshop/Customer.java
+++ src/com/mlenoir/homeshop/Customer.java
@@ -3,4 +3,12 @@ package com.mlenoir.homeshop;
 public class Customer {
     private String fullname;
     private String address;
+
+    public String getFullname() {
+        return this.fullname;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
 }
