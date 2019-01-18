c58fea112c039bc022045eed1ebdb051172e50b7
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-18 15:09:25 +0100

    Implement Product class of UML diagram
    To allow to compile the source code, I also implement the Bill class,
    since the 'buy' method requires an instance of this class as a
    parameter.

---
+++ src/com/mlenoir/homeshop/Product.java
@@ -0,0 +1,24 @@
+package com.mlenoir.homeshop;
+
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+
+    /**
+     * Display the full description of the product.
+     */
+    public void look() {
+
+    }
+
+    /**
+     * Add the product to a Bill.
+     *
+     * @param bill     The concerned bill.
+     * @param quantity The quantity to add.
+     */
+    public void buy(Bill bill, Integer quantity) {
+
+    }
+}
