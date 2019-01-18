b797eb34306ea042f50d6bba97dc3d977ae5c6cb
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-18 16:16:21 +0100

    Add getters/setters in Product

--- src/com/mlenoir/homeshop/Product.java
+++ src/com/mlenoir/homeshop/Product.java
@@ -5,6 +5,22 @@ public class Product {
     private String description;
     private double price;
 
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     /**
      * Display the full description of the product.
      */
