d96d09eb59d5acac65526bd09bad5a0d3910ffc3
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-21 11:01:07 +0100

    Add getInfo behavior to all Delivery
    This behavior is important to generate a bill.

--- src/com/mlenoir/homeshop/Delivery.java
+++ src/com/mlenoir/homeshop/Delivery.java
@@ -1,5 +1,7 @@
 package com.mlenoir.homeshop;
 
 public interface Delivery {
+    public String getInfo();
+
     public double getPrice();
 }
--- src/com/mlenoir/homeshop/TakeAwayDelivery.java
+++ src/com/mlenoir/homeshop/TakeAwayDelivery.java
@@ -2,6 +2,11 @@ package com.mlenoir.homeshop;
 
 public class TakeAwayDelivery implements Delivery {
     @Override
+    public String getInfo() {
+        return "Retrait gratuit à l'entrepôt dans Paris";
+    }
+
+    @Override
     public double getPrice() {
         return 0.0d;
     }
