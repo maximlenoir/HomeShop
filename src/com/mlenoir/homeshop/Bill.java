5ab1501441584fd11af8b681c4a84d17ded89661
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-21 12:06:23 +0100

    Add generate method's logic in Bill

--- src/com/mlenoir/homeshop/Bill.java
+++ src/com/mlenoir/homeshop/Bill.java
@@ -60,6 +60,42 @@ public class Bill {
      * @param writer The writer object in charge to writing.
      */
     public void generate(Writer writer) {
+        writer.start();
 
+        writer.writeLine("HomeShop compagnie");
+        writer.writeLine("1, Place Charles de Gaulle, 75008 Paris");
+
+        writer.writeLine("");
+
+        writer.writeLine("Facture à l'attention de :");
+        writer.writeLine(this.customer.getFullname());
+        writer.writeLine(this.customer.getAddress());
+
+        writer.writeLine("");
+
+        writer.writeLine("Mode de livraison : " + this.delivery.getInfo().toLowerCase());
+
+        writer.writeLine("");
+
+        writer.writeLine("Produits :");
+        writer.writeLine("-----------------------------------------------------");
+
+        for (Map.Entry<Product, Integer> entry : this.products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+
+            writer.writeLine(product.getName() + " - " + product.getPrice() + "€ - " + quantity + " unité(s)");
+            writer.writeLine(product.getDescription());
+
+            writer.writeLine("");
+        }
+
+        writer.writeLine("Livraison : " + this.delivery.getPrice() + "€");
+
+        writer.writeLine("-----------------------------------------------------");
+
+        writer.writeLine("Total : " + this.getTotal() + "€");
+
+        writer.stop();
     }
 }
