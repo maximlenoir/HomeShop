c6cfe67cdef865906283e3ff24af4af340cf1ea0
Maxime LENOIR <maxime.lenoir@aperam.com>
2019-01-22 11:58:23 +0100

    Add override methods in BillServlet

--- src/main/java/com/mlenoir/homeshop/web/BillServlet.java
+++ src/main/java/com/mlenoir/homeshop/web/BillServlet.java
@@ -1,7 +1,19 @@
 package com.mlenoir.homeshop.web;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class BillServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
